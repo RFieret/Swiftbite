2525abcf45077fb8c9f27d0b5cd525c49405f677
RFieret <494690@student.fontys.nl>
2025-05-12 14:14:14 +0200

    Removed Tests

--- Backend/Services/e2e-tests/src/test/java/com/e2e/tests/SimplifiedOrderTest.java
+++ Backend/Services/e2e-tests/src/test/java/com/e2e/tests/SimplifiedOrderTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Tag;
 @Tag("e2e")
 public class SimplifiedOrderTest {
 
-    @Test
-    public void testSomething() {
-        System.out.println("Running simplified order test");
-        // Just a simple test to verify test discovery works
-        assert(true);
-    }
+//    @Test
+//    public void testSomething() {
+//        System.out.println("Running simplified order test");
+//        // Just a simple test to verify test discovery works
+//        assert(true);
+//    }
 }
